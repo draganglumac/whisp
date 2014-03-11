07dcd7b37bdf708e93ab611ce7ae93f766571eea
AlexsJones <jonesax@hush.comm>
2014-03-11 07:21:32 +0000

    adding pretty picture

---
+++ src/net/passive.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  passive.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/11/14 07:21:12
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  jonesax (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "passive.h"
+void passive_listener_start(jnx_hashmap *configuration) {
+
+}
+void passive_listener_stop() {
+
+}
