bf41982fef0d80cd15d28135e80c1b1cb84467d7
AlexsJones <jonesax@hush.com>
2015-01-16 15:41:36 +0000

    resolved all build warnings

--- src/app/ipc.c
+++ src/app/ipc.c
@@ -21,23 +21,24 @@
 #include "ipc.h"
 static jnx_hashmap *configuration;
 
-int ipc_incoming_comms(uint8_t *out, size_t len, jnx_unix_socket *s) {
+jnx_uint32 ipc_incoming_comms(uint8_t *out, size_t len, jnx_unix_socket *s) {
 
-	printf("Received incoming --> %s\n",out);
 
-	return 0;
+  printf("Received incoming --> %s\n",out);
+
+  return 0;
 }
 void ipc_setup(jnx_hashmap *config) {
 
-	configuration = config;
-	JNX_LOG(DEFAULT_CONTEXT,"Starting Interprocess communications...\n");
-	jnx_unix_socket *inc_sock = jnx_unix_datagram_socket_create(jnx_hash_get(configuration,"IPC_LISTEN"));
-	jnx_hash_put(configuration,"IPC_LISTEN_SOCK",inc_sock);
-	jnx_unix_datagram_socket_listen(inc_sock,ipc_incoming_comms);
+  configuration = config;
+  JNX_LOG(DEFAULT_CONTEXT,"Starting Interprocess communications...\n");
+  jnx_unix_socket *inc_sock = jnx_unix_datagram_socket_create(jnx_hash_get(configuration,"IPC_LISTEN"));
+  jnx_hash_put(configuration,"IPC_LISTEN_SOCK",inc_sock);
+  jnx_unix_datagram_socket_listen(inc_sock,ipc_incoming_comms);
 
 }
 void ipc_teardown() {
 
-	jnx_unix_socket *s = jnx_hash_get(configuration,"IPC_LISTEN_SOCK");
-	jnx_unix_socket_destroy(&s);
+  jnx_unix_socket *s = jnx_hash_get(configuration,"IPC_LISTEN_SOCK");
+  jnx_unix_socket_destroy(&s);
 }
